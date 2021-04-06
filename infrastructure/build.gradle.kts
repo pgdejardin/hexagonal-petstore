2b33334068cea717e5d9e61e071ee3a284026b0e
Paul-Guillaume Déjardin <pgdejardin@xebia.fr>
2021-04-06 15:25:47 +0200

    chore(sqs): add aws dependency
    and config to infrastructure

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,4 +1,5 @@
 val http4kVersion: String by rootProject
+val config4kVersion: String by rootProject
 
 dependencies {
   implementation(project(":domain"))
@@ -7,6 +8,11 @@ dependencies {
   implementation("org.http4k:http4k-core")
   implementation("org.http4k:http4k-format-jackson")
   implementation("org.http4k:http4k-client-apache")
+
+  implementation("io.github.config4k:config4k:$config4kVersion")
+
+  implementation(platform("software.amazon.awssdk:bom:2.15.0"))
+  implementation("software.amazon.awssdk:sqs")
 
   testImplementation("org.http4k:http4k-testing-kotest")
 }
